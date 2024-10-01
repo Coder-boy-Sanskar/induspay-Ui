6838c44ffa6c2d27290ee0cda3c14817338f9d66
Sanskar Arora <sanskararora10282001@gmail.com>
2024-10-01 19:06:08 +0530

    basic code structure create, setup components and pages folder ...

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import HomePage from './pages/homePage/HomePage';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+     
+     <Header/>
+     <HomePage/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
